1d61ad164894dfe86103813661f1948899e123b9
vlad <vlad@vladevpayne.xyz>
2025-02-06 17:16:20 -0500

    attempt 2

--- plugin.rb
+++ plugin.rb
@@ -5,13 +5,15 @@
 # url: https://github.com/vladtheimplier/discourse-ssd
 
 after_initialize do
-  module TopicGuardian
-     def can_delete_topic?(topic)
+  require_dependency 'guardian'
+
+  module ::TopicGuardian
+    def can_delete_topic?(topic)
       !topic.trashed? &&
         (
           is_staff? || is_my_own?(topic) || is_category_group_moderator?(topic.category) ||
-            user&.in_any_groups?(SiteSetting.delete_all_posts_and_topics_allowed_groups_map)
+          user&.in_any_groups?(SiteSetting.delete_all_posts_and_topics_allowed_groups_map)
         ) && !topic.is_category_topic? && !Discourse.static_doc_topic_ids.include?(topic.id)
-     end
+    end
   end
 end
